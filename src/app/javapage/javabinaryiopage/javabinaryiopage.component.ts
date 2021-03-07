3e75f38647b0950420190b2433d00300be4f8b22
jfspps <jfsapps@gmail.com>
2021-03-07 23:05:48 +0000

    added Java binary I_O

--- src/app/javapage/javabinaryiopage/javabinaryiopage.component.ts
+++ src/app/javapage/javabinaryiopage/javabinaryiopage.component.ts
@@ -12,6 +12,71 @@ export class JavabinaryiopageComponent implements OnInit {
 
   lang = ["java"];
 
+  writeByteStreams = `public someFunc() throws IOException {
+    try (DataOutputStream someFile = new DataOutputStream(
+      new BufferedOutputStream(new FileOutputStream("someFileName.dat")))) {
+        for (...) {
+          someFile.writeInt(somePOJO.getIntValue);
+          someFile.writeUTF(somePOJO.getSomeString());
+        }
+    }
+  }`;
+
+  readByteStreams = `try(DataInputStream someFile = 
+    new DataInputStream(new BufferedInputStream(new FileInputStream("someFileName.dat")))) {
+    boolean eof = false;
+    while(!eof) {
+        try {
+            int someInt = someFile.readInt();
+            String someString = someFile.readUTF();
+            
+            System.out.println("Int found " someInt);
+            System.out.println("String found " + someString);            
+        } catch(EOFException e) {
+          // this ultimately forces the loop to terminate at
+            eof = true;
+        }
+    }
+} catch(IOException io) {
+    System.out.println("IO Exception: " + io.printStackTrace());
+}`;
+
+  serialisable = `public class SomeClass implements Serializable{
+   // SomeClass properties; all classes instanced here must also implement Serializable
+  private long serialVersionUID = 1L;
+  ...
+  `;
+
+  objectOutputStream = `try (ObjectOutputStream someFile = 
+    new ObjectOutputStream(
+      new BufferedOutputStream(new FileOutputStream("someFileName.dat")))) {
+    for(...) {
+      someFile.writeObject(objectInstance);
+    }
+}`;
+
+objectInputStream = `try(ObjectInputStream someFile = 
+  new ObjectInputStream(
+    new BufferedInputStream(new FileInputStream("someFileName.dat")))) {
+  boolean eof = false;
+  while(!eof) {
+      try {
+          POJO pojo = (POJO) someFile.readObject();
+          System.out.println("Got object with int " + pojo.getInt());
+          System.out.println("Got object with long " + pojo.getLong());
+          ...
+      } catch(EOFException e) {
+        // terminate the loop here
+          eof = true;
+      }
+  }
+} catch(IOException io) {
+  System.out.println("IO Exception" + io.getMessage());
+} catch(ClassNotFoundException e) {
+  System.out.println("ClassNotFoundException " + e.getMessage());
+}`;
+
+
   constructor() { }
 
   ngOnInit(): void {
