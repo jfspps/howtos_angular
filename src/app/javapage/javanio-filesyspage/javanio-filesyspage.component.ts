fa80f09139f3e358dd00cab85959f5384f411fb2
jfspps <jfsapps@gmail.com>
2021-03-11 22:37:13 +0000

    added working directory

--- src/app/javapage/javanio-filesyspage/javanio-filesyspage.component.ts
+++ src/app/javapage/javanio-filesyspage/javanio-filesyspage.component.ts
@@ -27,8 +27,20 @@ export class JavanioFilesyspageComponent implements OnInit {
     } catch(IOException e) {
         System.out.println(e.getMessage());
     }
-  }
-  `
+  }`;
+
+  workingDir = `filePath = Paths.get(".");
+  System.out.println(filePath.toAbsolutePath());`;
+
+  usingRelativeToWorking = `// find ./dir2/someFile.txt
+  Path path2 = FileSystems.getDefault().getPath(".", "dir2", "someFile.txt");
+
+  // find ../siblingDir/someFile.txt; note that .. moves up to the parent
+  Path path3 = FileSystems.getDefault().getPath("..", "siblingDir", "someFile.txt");
+
+  // print the full juxtaposed path with normalize()
+  System.out.println(path2.normalize().toAbsolutePath());
+  printFile(path2.normalize());`
 
   constructor() { }
 
