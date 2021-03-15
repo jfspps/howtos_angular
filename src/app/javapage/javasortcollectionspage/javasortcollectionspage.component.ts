4560d2941591a3da818304ede8463023dfa55d17
jfspps <jfsapps@gmail.com>
2021-03-15 18:20:03 +0000

    added Comparable, Comparator and Map

--- src/app/javapage/javasortcollectionspage/javasortcollectionspage.component.ts
+++ src/app/javapage/javasortcollectionspage/javasortcollectionspage.component.ts
@@ -17,6 +17,31 @@ export class JavasortcollectionspageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  compareTo = `@Override
+  public int compareTo(Seat seat) {
+      return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
+  }`;
+
+  comparator = `static final Comparator<Seat> PRICE_ORDER;
+
+  static {
+      PRICE_ORDER = new Comparator<Seat>() {
+          @Override
+          public int compare(Seat seat1, Seat seat2) {
+              if (seat1.getPrice() < seat2.getPrice()) {
+                  return -1;
+              } else if (seat1.getPrice() > seat2.getPrice()) {
+                  return 1;
+              } else {
+                  return 0;
+              }
+          }
+      };
+  }
+  
+  // then compare(seat1, seat2) is equivalent to
+  // seat1.getPrice().compareTo(seat2.getPrice())`;
+
   onHighlight(e) {
     this.response = {
       language: e.language,
