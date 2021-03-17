a6918c93bc516f29ab5f91711317ca057fb14216
jfspps <jfsapps@gmail.com>
2021-03-17 15:01:18 +0000

    updated Java Modules

--- src/app/javapage/javafxmodulespage/javafxmodulespage.component.ts
+++ src/app/javapage/javafxmodulespage/javafxmodulespage.component.ts
@@ -25,9 +25,37 @@ export class JavafxmodulespageComponent implements OnInit {
 
     opens com.BodyBand;
 
-    //required for JavaFX to access BodyBand DB methods and associated controllers
+    // required for JavaFX to access BodyBand DB methods and associated controllers
     opens com.BodyBand.model;
     opens com.BodyBand.controller;
+}`;
+
+controller = `module com.example.common {
+  requires javafx.base;
+
+  exports com.example.common;
+  opens com.example.common to javafx.base;
+}`;
+
+database = `module com.example.db {
+  requires java.sql;
+  requires sqlite.jdbc;
+  
+  // javafx.base also imported through transitive
+  requires transitive com.example.common;
+
+  exports com.example.db;
+}`;
+
+UI = `module com.example.ui {
+  requires javafx.fxml;
+  requires javafx.controls;
+
+  // javafx.base also imported through transitive
+  requires com.example.db;
+
+  exports com.example.ui to javafx.graphics;
+  opens com.example.ui to javafx.fxml;
 }`;
 
   onHighlight(e) {
