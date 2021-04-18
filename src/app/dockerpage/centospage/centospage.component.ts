652f463f4c421d296e131e96a2a27f8df43f3c8e
jfspps <jfsapps@gmail.com>
2021-04-18 21:59:58 +0100

    updated docker build command

--- src/app/dockerpage/centospage/centospage.component.ts
+++ src/app/dockerpage/centospage/centospage.component.ts
@@ -9,7 +9,7 @@ import { HighlightResult } from 'ngx-highlightjs';
 export class CentospageComponent implements OnInit {
 
   response: HighlightResult;
-  buildCentOS = `docker build -t spring-boot-docker`;
+  buildCentOS = `docker build -t spring-boot-docker .`;
   dockerFile = 
     `FROM centos \n RUN yum install -y java-11-openjdk-devel \n VOLUME /tmp \n ADD /spring-boot-web-0.0.1-SNAPSHOT.jar myApp.jar \n RUN sh -c 'touch /myApp.jar' \n ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/myApp.jar"]`
   
