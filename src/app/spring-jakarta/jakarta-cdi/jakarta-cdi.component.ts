098f610b0b10e3c457c39b1eeb40fc93d7be1811
jfspps <jfsapps@gmail.com>
2021-05-02 22:00:53 +0100

    added CDI qualifier intro

--- src/app/spring-jakarta/jakarta-cdi/jakarta-cdi.component.ts
+++ src/app/spring-jakarta/jakarta-cdi/jakarta-cdi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HighlightResult } from 'ngx-highlightjs';
 
 @Component({
   selector: 'app-jakarta-cdi',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JakartaCDIComponent implements OnInit {
 
+  response: HighlightResult;
+
+  lang = ["java"];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  commonInterface = `
+  public interface CommonInterface{
+  }`
+
+  cdiQualifiers = `// in annotationsPackagePath
+
+  @Qualifier
+  @Retention(RetentionPolicy.RUNTIME)
+  // set how the dependency can be injected:
+  @Target({ElementType.FIELD, ElementType.TYPE, ElementType.METHOD})
+  public @interface CustomBean {
+  }`;
+
+  injectHere = `
+  @Inject
+  @CustomBean
+  private CommonInterface someInstance;
+  `
+
+  qualifyBean = `
+  @annotationsPackagePath.CustomBean
+  public class SomeClass implements CommonInterface {
+    // some fields/methods unique to SomeClass
+  }`;
+
+  onHighlight(e) {
+    this.response = {
+      language: e.language,
+      relevance: e.relevance,
+      second_best: '{...}',
+      top: '{...}',
+      value: '{...}'
+    }
+  }
 }
