8c1eff0d61e610464bae7dc4c254aa7aff88e44f
jfspps <jfsapps@gmail.com>
2021-05-14 14:32:34 +0100

    adding basics of JPA entities

--- src/app/spring-jakarta/jakarta-jpa/jakarta-jpa.component.ts
+++ src/app/spring-jakarta/jakarta-jpa/jakarta-jpa.component.ts
@@ -17,6 +17,57 @@ export class JakartaJpaComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  jpaTable = `
+  @Entity
+  @Table(name = "Some_Table_Name")
+  public class Costs {
+
+    @Id
+    Long id;
+
+    private double income;
+
+    // getters and setters
+  }
+  `;
+
+  jpaSuperClass = `
+  // note that SomeAbstractEntityClass will not have its own table (this is handled 
+  // by child classes)
+  @MappedSuperClass
+  public abstract class SomeAbstractEntityClass implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+
+    @Version
+    protected Long version;
+
+    // other fields to be implemented go next
+
+    // getters and setters
+  }`
+
+  jpaChildClass = `
+  @Entity
+  public class ChildOne extends SomeAbstractEntityClass {
+
+    // add other fields, getters and setters that are unique to ChildOne
+  }
+  `
+
+  jpaChildClassOverride = `
+  @Entity
+  @AttributeOverride(name = "id", column = @Column(name = "childOneID"))
+  public class ChildOne extends SomeAbstractEntityClass {
+
+    // ChildOne instances JPA id is now referred to as childOneID
+
+    // add other fields, getters and setters that are unique to ChildOne
+  }
+  `
+
   onHighlight(e) {
     this.response = {
       language: e.language,
