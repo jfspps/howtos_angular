65ebacb0ab49b46df32a1c6d74125ac07299d969
jfspps <jfsapps@gmail.com>
2021-05-16 13:54:41 +0100

    added Mapped types and fetching

--- src/app/spring-jakarta/jakarta-jpa/jakarta-jpa.component.ts
+++ src/app/spring-jakarta/jakarta-jpa/jakarta-jpa.component.ts
@@ -88,6 +88,49 @@ export class JakartaJpaComponent implements OnInit {
   }
   `;
 
+  jpaEnums = `
+  public enum Status {
+    // each enum is zero-based, so VACANT is 0, OCCUPIED is 1 etc.
+    // this also means that the order of the list is important
+    VACANT,
+    OCCUPIED,
+    MAINTENANCE
+  }`;
+
+  jpaEnumsFields = `
+  public class SomeClass {
+
+    // assume to store ordinals of the enum
+    private Status currentStatus;
+
+
+    // assume to store strings of the enum
+    @Enumerated(EnumType.STRING)
+    private Status currentStatusString;
+
+    // only load someImage when the getter is called
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] someImage;
+  }`;
+
+  jpaEmbedded = `
+  @Embeddable
+  public class HelperClass {
+    // other fields and methods
+  }
+
+  // in some other class file...
+
+  public class MainClass {
+
+    @Embedded
+    private HelperClass embeddedClass;
+
+    // getters and setters etc.
+  }
+  `;
+
   onHighlight(e) {
     this.response = {
       language: e.language,
