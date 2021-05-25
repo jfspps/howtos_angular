c37d99dd39a54f9075e4c41e0f0b67c09499b098
jfspps <jfsppsDev@gmail.com>
2021-05-25 14:40:08 +0100

    updated DFS and BFS

--- src/app/javapage/java-dfs-and-bfs/java-dfs-and-bfs.component.ts
+++ src/app/javapage/java-dfs-and-bfs/java-dfs-and-bfs.component.ts
@@ -18,144 +18,94 @@ export class JavaDfsAndBfsComponent implements OnInit {
   }
 
   searchableTree = `
-  public class SearchableTree<T> {
+  public class SearchableGraph<T> {
 
     private final T data;
     private boolean visited;
 
-    private SearchableTree<T>[] children;
-    private SearchableTree<T>[] siblings;
+    // this applied to a more generic graph as opposed to a binary tree
+    private SearchableGraph<T>[] adjacent;
 
-    public SearchableTree(T data) {
+    public SearchableGraph(T data) {
         this.visited = false;
         this.data = data;
     }
 
-    public void setChildren(SearchableTree<T>[] children){
-        this.children = children;
+    public SearchableGraph<T>[] getAdjacent() {
+        return adjacent;
     }
 
-    public void setSiblings(SearchableTree<T>[] siblings){
-        this.siblings = siblings;
+    public void setAdjacent(SearchableGraph<T>[] adjacent) {
+        this.adjacent = adjacent;
     }
 
-    public SearchableTree<T>[] getSiblings(){
-        return  this.siblings;
-    }
-
-    public SearchableTree<T>[] getChildren(){
-        return this.children;
-    }
-
-    public boolean hasBeenVisited(){
+    public boolean hasBeenVisited() {
         return this.visited;
     }
 
-    public void setVisited(boolean visited){
+    public void setVisited(boolean visited) {
         this.visited = visited;
     }
 
     // can be reviewed as required
-    public void printData(SearchableTree<T> node){
+    public void printData(SearchableGraph<T> node) {
         System.out.println(node.data);
     }
 
-    // traverses a tree following a depth-based approach 
-    // (visit child nodes before peers)
-    // like BFS, DFS is O(n)
-    public void depthFirstSearch(SearchableTree<T> root){
-        if (root == null){
+    // traverses a tree following a depth-based approach; like BFS, DFS is O(n)
+    public String depthFirstSearch(SearchableGraph<T> root, StringBuilder stringBuilder) {
+        if (root == null) {
             System.out.println("Given node is null");
-            return;
+            return "Null graph received";
         }
 
-        // in considering recursion, the newly found node is printed
-        // before visiting it's child nodes (hence pre-order)
-        printData(root);
+        // in considering recursion, the newly found node is printed before 
+        // visiting it's "child" nodes (hence pre-order)
+        stringBuilder.append(root.data).append(" ");
         root.visited = true;
 
-        if (root.children != null && root.children.length != 0){
-            for (SearchableTree<T> tree : root.children){
-                // check if visited otherwise for cyclic graphs 
-                // one would end up in an infinite loop
-                if (!tree.visited){
-                    depthFirstSearch(tree);
-                }
+        for (SearchableGraph<T> tree : root.adjacent) {
+            // check if visited otherwise for cyclic graphs one would 
+            // end up in an infinite loop
+            if (!tree.visited) {
+                depthFirstSearch(tree, stringBuilder);
             }
         }
+
+        return stringBuilder.toString();
     }
 
-    // build a queue of neighbouring nodes using rootsNeighbours and 
-    // visit them once all are queued
-    // like DFS, BFS is O(n)
-    public void breadthFirstSearch(
-      SearchableTree<T> root, SearchableTree<T>[] rootsNeighbours){
-        Queue neighbouringNodes = new Queue();
-        root.visited = true;
-        neighbouringNodes.enqueue(root);    // FIFO, root handled first
-
-        if (rootsNeighbours == null){
-            System.out.println(root.data);
-            return;
+    // build a queue of neighbouring nodes using queuedNodes and 
+    // visit them once all are queued; like DFS, BFS is O(n)
+    public String breadthFirstSearch(SearchableGraph<T> root, StringBuilder stringBuilder) {
+        if (root == null) {
+            System.out.println("Given node is null");
+            return "Null graph received";
         }
 
-        while (!neighbouringNodes.isEmpty()){
-            // initially, this returns root and then adds its siblings
-            // to the queue the for loop should not repeat itself for 
-            // each sibling since the flag node.visited is true
-            SearchableTree<T> currentNode = 
-                (SearchableTree<T>) neighbouringNodes.dequeue();
-            printData(currentNode);
+        Queue queuedNodes = new Queue();
+        root.visited = true;
+        queuedNodes.enqueue(root);    // FIFO, root handled first
 
-            // check if visited otherwise for cyclic graphs
-            // one would end up in an infinite loop
-            for (SearchableTree<T> node : rootsNeighbours){
-                if (!node.visited){
+        while (!queuedNodes.isEmpty()) {
+            // initially, this returns root and then adds its adjacent nodes 
+            // to the queue; the for loop should not repeat itself for each
+            // adjacent node since the flag node.visited is true
+            SearchableGraph<T> currentNode = (SearchableGraph<T>) queuedNodes.dequeue();
+            stringBuilder.append(currentNode.data).append(" ");
+
+            // check if visited otherwise for cyclic graphs one would end up in an infinite loop
+            for (SearchableGraph<T> node : currentNode.getAdjacent()) {
+                if (!node.visited) {
                     node.visited = true;
-                    neighbouringNodes.enqueue(node);
-                }
-            }
-        }
-    }
-
-    // modified breadthFirstSearch(), check the node and its 
-    // siblings to see if they have been visited elsewhere
-    public boolean hasBeenVisitedByOthers(SearchableTree<T> root,
-       SearchableTree<T>[] rootsNeighbours){
-        System.out.println("Processing " + root.data + "...");
-
-        if (root.visited){
-            System.out.println(
-              "Someone's been at " + root.data + " before; pathway present");
-            return true;
-        }
-
-        Queue neighbouringNodes = new Queue();
-        root.visited = true;
-        neighbouringNodes.enqueue(root);
-
-        while (rootsNeighbours != null && !neighbouringNodes.isEmpty()){
-            SearchableTree<T> currentNode =
-               (SearchableTree<T>) neighbouringNodes.dequeue();
-            printData(currentNode);
-
-            for (SearchableTree<T> node : rootsNeighbours){
-                if (node.visited){
-                    System.out.println(
-                      "Someone's been at " + node.data + " before; pathway present");
-                    return true;
-                } else {
-                    node.visited = true;
-                    neighbouringNodes.enqueue(node);
+                    queuedNodes.enqueue(node);
                 }
             }
         }
 
-        // by now all nodes have been explored
-        return false;
-    }
-}
-  `;
+        return stringBuilder.toString();
+        }
+    }`;
   onHighlight(e) {
     this.response = {
       language: e.language,
