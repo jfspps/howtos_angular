123ebdc784a18969a41ffe3e9458041778f50b02
jfspps <jfsppsDev@gmail.com>
2021-06-01 15:46:29 +0100

    added exception mappers

--- src/app/spring-jakarta/jakarta-jaxrsresmapfields/jakarta-jaxrsresmapfields.component.ts
+++ src/app/spring-jakarta/jakarta-jaxrsresmapfields/jakarta-jaxrsresmapfields.component.ts
@@ -35,7 +35,7 @@ export class JakartaJaxrsresmapfieldsComponent implements OnInit {
     public Response getObjects() {
 
       return Response.ok(someService.findAll())
-          .status(Status.FOUND)   // there are many other HTTP statuses
+          .status(Response.Status.OK)   // there are many other HTTP statuses
           .build;
     }
 
@@ -43,7 +43,7 @@ export class JakartaJaxrsresmapfieldsComponent implements OnInit {
     @Path("employee/{id})
     public Response getObjectById(@PathParam("id") Long id){
       return Response.ok(someService.findById(id))
-          .status(Status.FOUND)
+          .status(Response.Status.OK)
           .build;
     }
 
@@ -55,7 +55,66 @@ export class JakartaJaxrsresmapfieldsComponent implements OnInit {
         object.getId().toString()
           ).build();
 
-      Response.created(uri)
+      // the built URI can be sent back to the web service (in 
+      // agreement with the above getObjectById())
+      return Response.created(uri)
+          .status(Response.Status.CREATED)
+          .build();
+    }
+  }
+  `;
+
+  exceptionMapper = `
+  // inform JAX-RS that this is should be available at runtime
+  @Provider
+  public class ExceptionMapperConstraintViolation 
+      implements ExceptionMapper<ConstraintViolationException> {
+
+    // add all violations to a Map and return
+    @Override
+    public Response toResponse(ConstraintViolationException exc) {
+
+      final Map<String, String> constraintViolations = new HashMap<>();
+
+      for (ConstraintViolation violation : exc.geConstraintViolations()){
+        String path = violation.getPropertyPath().toString();
+        constraintViolations.put(path, violation.getMessage());
+      }
+
+      return Response.status(Response.Status.PRECONDITION_FAILED)
+          .entity(constraintViolations)
+          .build;
+    }
+  }`;
+
+  validateAtResourceLevel = `
+  @Path("somewhere")
+  @Produces("application/json")
+  @Consumes("application/json")
+  public class SomeResource {
+
+    @Inject
+    SomeService someService;
+
+    // inject the interface UriInfo (see POST)
+    @Context
+    private UriInfo uriInfo;
+
+    // ... other resource methods ...
+
+    @POST
+    @Path("newObject")
+    public Response newObject(@Valid SomeObject someObject){
+
+      // this method block is only run when validation is satisfactory
+
+      someService.save(someObject);
+      
+      URI uri = uriInfo.getAbsolutePathBuilder().path(
+        object.getId().toString()
+          ).build();
+
+      return Response.created(uri)
           .status(Response.Status.CREATED)
           .build();
     }
